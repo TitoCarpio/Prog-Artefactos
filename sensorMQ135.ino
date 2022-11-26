09c4899db9b64377bd6e386b3e2e41676ae93f40
TitoCarpio <00226620@uca.edu.sv>
2022-11-26 16:25:33 -0600

    Medicion del sensor sin libreria

--- sensorMQ135.ino
+++ sensorMQ135.ino
@@ -18,22 +18,36 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 // Libreria para mandar correos desde esp8266
-#include <EMailSender.h>
-#include <MQ135.h>
+//#include <EMailSender.h>
+// #include <MQ135.h>
+// Librerias para el uso de OLED
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
 
 
 char auth[] = BLYNK_AUTH_TOKEN;
 
-#define PIN_MQ135 A0
-MQ135 mq135_sensor(PIN_MQ135);
+/* Uncomment the initialize the I2C address , uncomment only one, If you get a totally blank screen try the other*/
+#define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
 
-//int sensor = A0;
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1   //   QT-PY / XIAO
+Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+// #define PIN_MQ135 A0
+// MQ135 mq135_sensor(PIN_MQ135);
+
+int sensor = A0;
 float valor= 0.0;
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "NO DISPONIBLE";
-char pass[] = "28051999*#";
+char ssid[] = "LookingPlace";
+char pass[] = "contrasenna123";
 
 BlynkTimer timer;
 
@@ -68,7 +82,8 @@ BLYNK_CONNECTED()
 
 void setup()
 {
- // pinMode(sensor, INPUT);
+  pinMode(sensor, INPUT);
+  pinMode(D7, OUTPUT);
   // Debug console
   Serial.begin(115200);
 
@@ -79,22 +94,69 @@ void setup()
 
   // Setup a function to be called every second
   //timer.setInterval(1000L, myTimerEvent);
+
+  // Inicializacion de display OLED
+  delay(250); // wait for the OLED to power up
+  display.begin(i2c_Address, true); // Address 0x3C default
+  display.setContrast (255); // dim display
+
+  // Clear the buffer.
+  display.clearDisplay();
+  display.display();
 }
 
 void loop()
 {
+  digitalWrite(D7, LOW);
+  
   Blynk.run();
   timer.run();
   //valor = analogRead(sensor);
-  valor = mq135_sensor.getPPM();
+  valor = analogRead(sensor);
   Serial.println(valor);
-  if(valor > 220){
+  if(valor > 100){
     Blynk.logEvent("hppm", String("Peligroooo ")+ valor);
+    digitalWrite(D7, HIGH);
   }
   Blynk.virtualWrite(V0,valor);
-  delay(5000);
+  delay(2000);
   // You can inject your own code or combine it with other sketches.
   // Check other examples on how to communicate with Blynk. Remember
   // to avoid delay() function!
+
+  // Limpiar buffer
+  display.clearDisplay();
+ 
+  // Tamaño del texto
+  display.setTextSize(1);
+  // Color del texto
+  display.setTextColor(SH110X_WHITE);
+  // Posición del texto
+  display.setCursor(10, 10);
+  // Escribir texto
+  display.println("Calidad de Aire: ");
+
+  // Tamaño del texto
+  display.setTextSize(2);
+  // Color del texto
+  display.setTextColor(SH110X_WHITE);
+  // Posición del texto
+  display.setCursor(10, 32);
+  // Escribir texto
+  display.println(valor);
+
+  // Tamaño del texto
+  display.setTextSize(1);
+  // Color del texto
+  display.setTextColor(SH110X_WHITE);
+  // Posición del texto
+  display.setCursor(110, 40);
+  // Escribir texto
+  display.println("ppm");
+ 
+  // Enviar a pantalla
+  display.display();
+  
 }
+
 
